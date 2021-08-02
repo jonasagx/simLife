9be8f70211c42e4f38f42a16724d4a882b8a4c6d
Jonas Galvão Xavier <jonas.agx@gmail.com>
2021-08-01 23:35:45 -0700

    floating straight move
    hollow biotas

--- src/main.js
+++ src/main.js
@@ -11,42 +11,53 @@ function random(min, max) {
   return num;
 }
 
-const planctonSize = 50;
+let biotas = [];
+let planctons = [];
+
+const planctonSize = 5;
 class Plancton {
   constructor() {
     this.x = random(0 + planctonSize, width);
     this.y = random(0 + planctonSize, height);
     this.color = "RGBA(255, 255, 0, 1)";
+    this.size = planctonSize;
   }
 
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.arc(this.x, this.y, planctonSize, 0, 2 * Math.PI);
     ctx.fill();
-     // ctx.ellipse(this.x, this.y, planctonSize, planctonSize, Math.PI / 4, 0, 2 * Math.PI);
-    // ctx.stroke();
   }
+}
+
+while (planctons.length < 10) {
+  planctons.push(new Plancton());
 }
 
 class Biota {
   constructor() {
-    this.limbs = random(5, 40);
+    this.limbs = random(5, 20);
     this.size = this.limbs;
-    this.velX = 0.1 * this.limbs;
-    this.velY = 0.1 * this.limbs;
+    this.velX = 0.05 * this.limbs;
+    this.velY = 0.05 * this.limbs;
     this.color = "rgb(" + random(0, 255) + "," + random(0, 255) + "," +
       random(0, 255) + ")";
     this.x = random(0 + this.size, width);
     this.y = random(0 + this.size, height);
+    this.energy = 0;
   }
+
   draw() {
     ctx.beginPath();
-    ctx.fillStyle = this.color;
+    // ctx.fillStyle = this.color;
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
+    ctx.stroke();
   }
-  update() {
+
+  move() {
     if ((this.x + this.size) >= width) {
       this.velX = -(this.velX);
     }
@@ -63,39 +74,30 @@ class Biota {
       this.velY = -(this.velY);
     }
 
-    this.x += this.velX;
-    this.y += this.velY;
+    this.x += (this.velX + random(-1, 1));
+    this.y += (this.velY + random(-1, 1));
   }
-  // define ball collision detection
-  collisionDetect() {
-    for (let j = 0; j < biotas.length; j++) {
-      if (!(this === biotas[j])) {
-        const dx = this.x - biotas[j].x;
-        const dy = this.y - biotas[j].y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < this.size + biotas[j].size) {
-          biotas[j].color = this.color = "rgb(" + random(0, 255) + "," +
-            random(0, 255) + "," + random(0, 255) + ")";
-        }
+  eat() {
+    for (let j = 0; j < planctons.length; j++) {
+      const dx = this.x - planctons[j].x;
+      const dy = this.y - planctons[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + planctons[j].size) {
+        this.energy += planctons[j].size;
+        planctons.splice(j, 1);
+        break;
       }
     }
-  }
 }
 
-let biotas = [];
-let planctons = [];
-
+}
 while (biotas.length < 10) {
   biotas.push(new Biota());
 }
 
-while (planctons.length < 10) {
-  planctons.push(new Plancton());
-}
-
 // define loop that keeps drawing the scene constantly
-
 function loop() {
   ctx.fillStyle = "rgba(0,0,0,0.25)";
   ctx.fillRect(0, 0, width, height);
@@ -104,10 +106,10 @@ function loop() {
     p.draw()
   });
 
-  biotas.forEach(b => {
+ biotas.forEach(b => {
     b.draw();
-    b.update();
-    b.collisionDetect();
+    b.move();
+    b.eat();
   })
   requestAnimationFrame(loop);
 }
